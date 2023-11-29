bb0290a149f9d887aefcb17b69489f90f579b279
vanrogh <k.elaman2002@gmail.com>
2023-11-29 13:55:21 +0600

    awdawd

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import MagicMock
+from .management.commands.run_telegram_bot import start, recipes, add_recipe, help_command
 
-# Create your tests here.
+
+class TestBot(unittest.TestCase):
+
+    def test_start(self):
+        message = MagicMock()
+        message.chat.id = 564671702
+        start(message)
+
+    def test_recipes(self):
+        message = MagicMock()
+        message.chat.id = 564671702
+        recipes(message)
+
+    def test_add_recipe(self):
+        message = MagicMock()
+        message.chat.id = 564671702
+        add_recipe(message)
+
+    def test_help(self):
+        message = MagicMock()
+        message.chat.id = 564671702
+        help_command(message)
+
+
+if __name__ == '__main__':
+    unittest.main()
